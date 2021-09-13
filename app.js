f92aaa519a6ff20b3991e1d66fd121dc8401939d
jazzmbarry <jazzmbarry@gmail.com>
2021-09-13 17:45:08 -0600

    validate input

--- app.js
+++ app.js
@@ -9,12 +9,28 @@ const promptUser = () => {
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your name!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub Username!');
+              return false;
+            }
+          }
       },
       {
         type: 'input',
@@ -38,12 +54,28 @@ return inquirer.prompt([
     {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)' ,
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter your Project Name!');
+              return false;
+            }
+          }
     },
     {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project (Required)'
+        message: 'Provide a description of the project (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log('Please enter a description for your Project');
+              return false;
+            }
+          }
     },
     {
         type: 'checkbox',
